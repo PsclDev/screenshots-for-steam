e69808de66a646fb52c8026b0eda5771e18587f3
Pascal <github@unrundead.eu>
2019-01-26 19:28:36 +0100

    Functions.cs
    Methods Surrounded with Try Catch

--- Converter/Converter/Functions.cs
+++ Converter/Converter/Functions.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using BespokeFusion;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 
 namespace Converter
 {
@@ -11,50 +13,64 @@ namespace Converter
         private static int ConvertedFiles = 1;
         public static void SortScreenshots(string ImpPath)
         {
-            GetScreenshots = Directory.GetFiles(ImpPath, "*.png*.jpg");
-
-            for (int i = 1; i < GetScreenshots.Length; i++)
+            try
             {
-                string file = GetScreenshots[i];
+                GetScreenshots = Directory.EnumerateFiles(ImpPath, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".png") || s.EndsWith(".jpg")).ToArray();
 
-                DateTime temp = File.GetCreationTime(file);
-
-                int j = i - 1;
-
-                DateTime next = File.GetCreationTime(GetScreenshots[j]);
-
-                while (j >= 0 && next > temp)
+                for (int i = 1; i < GetScreenshots.Length; i++)
                 {
-                    GetScreenshots[j + 1] = GetScreenshots[j];
-                    j--;
-                }
+                    string file = GetScreenshots[i];
 
-                GetScreenshots[j + 1] = file;
+                    DateTime temp = File.GetCreationTime(file);
+
+                    int j = i - 1;
+
+                    DateTime next = File.GetCreationTime(GetScreenshots[j]);
+
+                    while (j >= 0 && next > temp)
+                    {
+                        GetScreenshots[j + 1] = GetScreenshots[j];
+                        j--;
+                    }
+
+                    GetScreenshots[j + 1] = file;
+                }
+            }
+            catch (Exception ex)
+            {
+                MaterialMessageBox.Show(ex.ToString(), "ERROR OCCURED");
             }
         }
         public static void ConvertAndExport(string SteamPath)
         {
-            foreach (string file in GetScreenshots)
+            try
             {
-                DateTime time = File.GetCreationTime(file);
-                string name = time.ToString("yyyy-MM-dd") + "_0000" + ConvertedFiles.ToString();
-                string Steam = $"{SteamPath}\\{name}.jpg";
-                string SteamVR = $"{SteamPath}\\{name}_vr.jpg";
+                foreach (string file in GetScreenshots)
+                {
+                    DateTime time = File.GetCreationTime(file);
+                    string name = time.ToString("yyyy-MM-dd") + "_0000" + ConvertedFiles.ToString();
+                    string Steam = $"{SteamPath}\\{name}.jpg";
+                    string SteamVR = $"{SteamPath}\\{name}_vr.jpg";
 
-                Image png = png = Image.FromFile(file);
-                png.Save(Steam, System.Drawing.Imaging.ImageFormat.Jpeg);
-                png.Save(SteamVR, System.Drawing.Imaging.ImageFormat.Jpeg);
-                png.Dispose();
+                    Image png = png = Image.FromFile(file);
+                    png.Save(Steam, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    png.Save(SteamVR, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    png.Dispose();
 
-                File.SetCreationTime(Steam, File.GetCreationTime(file));
-                File.SetLastWriteTime(Steam, File.GetLastWriteTime(file));
+                    File.SetCreationTime(Steam, File.GetCreationTime(file));
+                    File.SetLastWriteTime(Steam, File.GetLastWriteTime(file));
 
-                File.SetCreationTime(SteamVR, File.GetCreationTime(file));
-                File.SetLastWriteTime(SteamVR, File.GetLastWriteTime(file));
+                    File.SetCreationTime(SteamVR, File.GetCreationTime(file));
+                    File.SetLastWriteTime(SteamVR, File.GetLastWriteTime(file));
 
-                ConvertedFiles++;
+                    ConvertedFiles++;
 
-                File.Delete(file);
+                    File.Delete(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                MaterialMessageBox.Show(ex.ToString(), "ERROR OCCURED");
             }
         }
     }
